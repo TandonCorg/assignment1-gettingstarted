50fce9bfde5d81f0d77d8df17e3bf3109138313e
TandonCorg <ch5474@nyu.edu>
2026-01-29 17:37:55 -0600

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -2,10 +2,14 @@
 ### Input - All nine questions given in the assignment.
 ### Output - The right answer for the specific question.
 
+### welcome_assignment_answers
+### Input - All nine questions given in the assignment.
+### Output - The right answer for the specific question.
+
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
@@ -34,8 +38,7 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "Are
-    encoding and encryption the same? - Yes/No"
+    debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
@@ -48,4 +51,5 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
 

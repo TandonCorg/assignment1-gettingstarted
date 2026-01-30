f22aa617d99b6c43d55b3b3646d56e02ba40dc2f
TandonCorg <ch5474@nyu.edu>
2026-01-29 18:18:22 -0600

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -27,12 +27,12 @@ def welcome_assignment_answers(question):
         answer = 5
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to?  - The answer should be an integer number":
         answer = 3
-    #else: 
+    else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        #answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
-    #return(answer)
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    return(answer)
 # Complete all the questions.
 
 
@@ -51,6 +51,7 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
 
 
 

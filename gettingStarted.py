e8c749a4ca07c106edab8c89b40744f3733c6ee9
TandonCorg <ch5474@nyu.edu>
2026-01-29 18:16:48 -0600

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -32,7 +32,7 @@ def welcome_assignment_answers(question):
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
         #answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
-    return(answer)
+    #return(answer)
 # Complete all the questions.
 
 
@@ -51,6 +51,7 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
 
 
 
